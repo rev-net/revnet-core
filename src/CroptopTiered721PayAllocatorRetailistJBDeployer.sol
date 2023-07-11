293654dae2f157e93537058cfaebd71ca15260e6
mejango <mejango@protonmail.com>
2023-07-11 13:00:55 -0400

    Added croptop deployer

--- src/CroptopTiered721PayAllocatorRetailistJBDeployer.sol
+++ src/CroptopTiered721PayAllocatorRetailistJBDeployer.sol
@@ -1,45 +1,49 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.20;
 
-import {IJBTiered721Delegate} from "@jbx-protocol/juice-721-delegate/contracts/interfaces/IJBTiered721Delegate.sol";
+import { CroptopPublisher } from "@croptop/publisher/src/CroptopPublisher.sol";
+import { JB721Operations } from "@jbx-protocol/juice-721-delegate/contracts/libraries/JB721Operations.sol"; 
 import {IJBTiered721DelegateDeployer} from
     "@jbx-protocol/juice-721-delegate/contracts/interfaces/IJBTiered721DelegateDeployer.sol";
 import {JBDeployTiered721DelegateData} from
     "@jbx-protocol/juice-721-delegate/contracts/structs/JBDeployTiered721DelegateData.sol";
 import {IJBController3_1} from "@jbx-protocol/juice-contracts-v3/contracts/interfaces/IJBController3_1.sol";
 import {IJBDirectory} from "@jbx-protocol/juice-contracts-v3/contracts/interfaces/IJBDirectory.sol";
+import {IJBOperatable} from "@jbx-protocol/juice-contracts-v3/contracts/interfaces/IJBOperatable.sol";
 import {IJBPayoutRedemptionPaymentTerminal3_1_1} from
     "@jbx-protocol/juice-contracts-v3/contracts/interfaces/IJBPayoutRedemptionPaymentTerminal3_1_1.sol";
-import {IJBPayDelegate3_1_1} from "@jbx-protocol/juice-contracts-v3/contracts/interfaces/IJBPayDelegate3_1_1.sol";
+import {JBOperatorData} from "@jbx-protocol/juice-contracts-v3/contracts/structs/JBOperatorData.sol";
 import {JBPayDelegateAllocation3_1_1} from
     "@jbx-protocol/juice-contracts-v3/contracts/structs/JBPayDelegateAllocation3_1_1.sol";
 import {JBProjectMetadata} from "@jbx-protocol/juice-contracts-v3/contracts/structs/JBProjectMetadata.sol";
-import {BasicRetailistJBParams, PayAllocatorRetailistJBDeployer} from "./PayAllocatorRetailistJBDeployer.sol";
+import {BasicRetailistJBParams, Tiered721PayAllocatorRetailistJBDeployer} from "./Tiered721PayAllocatorRetailistJBDeployer.sol";
 
-/// @notice A contract that facilitates deploying a basic Retailist treasury that also can mint tiered 721s.
-contract Tiered721PayAllocatorRetailistJBDeployer is PayAllocatorRetailistJBDeployer {
+/// @notice A contract that facilitates deploying a basic Retailist treasury that also can mint tiered 721s via a Croptop proxy.
+contract CroptopTiered721PayAllocatorRetailistJBDeployer is Tiered721PayAllocatorRetailistJBDeployer {
 
-    /// @notice The directory of terminals and controllers for projects.
-    IJBDirectory public immutable directory;
-
-    /// @notice The contract responsible for deploying the delegate.
-    IJBTiered721DelegateDeployer public immutable delegateDeployer;
+    /// @notice The Croptop publisher that facilitates the permissioned publishing of NFT posts to a Juicebox project.
+    CroptopPublisher public publisher;
+    
+    /// @notice The permissions that the provided _operator should be granted. This is set once in the constructor to contain only the SET_SPLITS operation.
+    uint256[] public croptopPermissionIndexes;
 
     /// @param _directory The directory of terminals and controllers for projects.
     /// @param _delegateDeployer The delegate deployer.
+    /// @param _publisher The Croptop publisher that facilitates the permissioned publishing of NFT posts to a Juicebox project.
     /// @param _controller The controller that projects are made from.
     /// @param _terminal The terminal that projects use to accept payments from.
     constructor(
+        CroptopPublisher _publisher,
         IJBDirectory _directory,
         IJBTiered721DelegateDeployer _delegateDeployer,
         IJBController3_1 _controller,
         IJBPayoutRedemptionPaymentTerminal3_1_1 _terminal
-    ) PayAllocatorRetailistJBDeployer(_controller, _terminal) {
-        directory = _directory;
-        delegateDeployer = _delegateDeployer;
+    ) Tiered721PayAllocatorRetailistJBDeployer(_directory, _delegateDeployer, _controller, _terminal) {
+        publisher = _publisher;
+        croptopPermissionIndexes.push(JB721Operations.ADJUST_TIERS);
     }
 
-    /// @notice Deploy a project with basic Retailism constraints that includes Tiered 721s and also calls other pay delegates that are specified.
+    /// @notice Deploy a project with basic Retailism constraints that includes Tiered 721s that the Croptop publisher can facilitate permissioned posts to, and also calls other pay delegates that are specified.
     /// @param _operator The address that will receive the token premint, initial reserved token allocations, and who is allowed to change the allocated reserved rate distribution.
     /// @param _projectMetadata The metadata containing project info.
     /// @param _name The name of the ERC-20 token being create for the project.
@@ -48,7 +52,7 @@ contract Tiered721PayAllocatorRetailistJBDeployer is PayAllocatorRetailistJBDepl
     /// @param _otherDelegateAllocations Any pay delegate allocations that should run when the project is paid.
     /// @param _extraFundingCycleMetadata Extra metadata to attach to the funding cycle for the delegates to use.
     /// @return projectId The ID of the newly created Retailist project.
-    function deployTiered721PayAllocatorProjectFor(
+    function deployCroptopTiered721PayAllocatorProjectFor(
         address _operator,
         JBProjectMetadata memory _projectMetadata,
         string memory _name,
@@ -58,37 +62,15 @@ contract Tiered721PayAllocatorRetailistJBDeployer is PayAllocatorRetailistJBDepl
         JBPayDelegateAllocation3_1_1[] calldata _otherDelegateAllocations,
         uint8 _extraFundingCycleMetadata
     ) public returns (uint256 projectId) {
-        // Get the project ID, optimistically knowing it will be one greater than the current count.
-        projectId = directory.projects().count() + 1;
-
-        // Deploy the delegate contract.
-        IJBTiered721Delegate _delegate =
-            delegateDeployer.deployDelegateFor(projectId, _deployTiered721DelegateData, directory);
-
-        // Keep a reference to the number of delegate allocations passed in.
-        uint256 _numberOfOtherDelegateAllocations = _otherDelegateAllocations.length;
-
-        // Track an updated list of delegate allocations that'll also fit the Tiered 721 Delegate allocation.
-        JBPayDelegateAllocation3_1_1[] memory _delegateAllocations =
-            new JBPayDelegateAllocation3_1_1[](_numberOfOtherDelegateAllocations + 1);
-
-        // Repopulate the updated list with the params passed in.
-        for (uint256 _i; _i < _numberOfOtherDelegateAllocations;) {
-            _delegateAllocations[_i] = _otherDelegateAllocations[_i];
-            unchecked {
-                ++_i;
-            }
-        }
-
-        // Add the Tiered 721 Allocation at the end.
-        _delegateAllocations[_numberOfOtherDelegateAllocations] = JBPayDelegateAllocation3_1_1({
-            delegate: IJBPayDelegate3_1_1(address(_delegate)),
-            amount: 0,
-            metadata: bytes("")
-        });
-
-        super.deployPayAllocatorProjectFor(
-            _operator, _projectMetadata, _name, _symbol, _data, _delegateAllocations, _extraFundingCycleMetadata
+        projectId = super.deployTiered721PayAllocatorProjectFor(
+            _operator, _projectMetadata, _name, _symbol, _data, _deployTiered721DelegateData, _otherDelegateAllocations, _extraFundingCycleMetadata
         );
+
+        // Give the croptop publisher permission to post on this contract's behalf.
+        IJBOperatable(address(directory)).operatorStore().setOperator(JBOperatorData({
+          operator: address(publisher),
+          domain: projectId,
+          permissionIndexes: croptopPermissionIndexes
+        })); 
     }
 }
