297a2895a3ba6566cea75766e0f294ab40b67ea6
mejango <mejango@protonmail.com>
2023-12-18 22:07:49 -0500

    deployed

--- script/Deploy.s.sol
+++ script/Deploy.s.sol
@@ -10,30 +10,39 @@ import {REVBasicDeployer} from "src/REVBasicDeployer.sol";
 contract Deploy is Script {
     function run() public {
         uint256 chainId = block.chainid;
-        address controllerAddress;
-
+        string memory chain;
         // Ethereun Mainnet
         if (chainId == 1) {
-            controllerAddress = address(0);
+            chain = "1";
             // Ethereum Sepolia
         } else if (chainId == 11_155_111) {
-            controllerAddress = 0x876437e4237017d2178022d1352A59be661C4142;
+            chain = "11155111";
             // Optimism Mainnet
         } else if (chainId == 420) {
-            controllerAddress = address(0);
+            chain = "420";
             // Optimism Sepolia
         } else if (chainId == 11_155_420) {
-            controllerAddress = 0x0227b76E082c635887ec58BaCaabAcC86934fe1c;
+            chain = "11155420";
             // Polygon Mainnet
-        // } else if (chainId == 137) {
-        //     // Polygon Mumbai
-        // } else if (chainId == 80_001) {
+        } else if (chainId == 137) {
+            chain = "137";
+            // Polygon Mumbai
+        } else if (chainId == 80_001) {
+            chain = "80001";
         } else {
             revert("Invalid RPC / no juice contracts deployed on this network");
         }
 
-        // address controllerAddress = address(123);
-        // emit k(controllerAddress);
+        address controllerAddress = 
+            stdJson.readAddress(
+            vm.readFile(
+                string.concat(
+                    "lib/juice-contracts-v4/broadcast/Deploy.s.sol/", chain, "/run-latest.json"
+                )
+            ),
+            ".transactions[7].contractAddress"
+        );
+
         vm.broadcast();
         new REVBasicDeployer(IJBController(controllerAddress));
     }
