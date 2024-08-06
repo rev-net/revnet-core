2f986c23bf4facbd6ac7e834ebfbbc88f9ca3aec
nowonder <justinpulley92@gmail.com>
2024-08-06 22:13:11 +0000

    test deploy with loans contract

--- test/RevLoan.t.sol
+++ test/RevLoan.t.sol
@@ -16,6 +16,7 @@ import "@bananapus/buyback-hook/script/helpers/BuybackDeploymentLib.sol";
 
 import {JBConstants} from "@bananapus/core/src/libraries/JBConstants.sol";
 import {JBAccountingContext} from "@bananapus/core/src/structs/JBAccountingContext.sol";
+import {REVLoans} from "../src/REVLoans.sol";
 import {REVStageConfig, REVAutoMint} from "../src/structs/REVStageConfig.sol";
 import {REVLoanSource} from "../src/structs/REVLoanSource.sol";
 import {REVDescription} from "../src/structs/REVDescription.sol";
@@ -39,6 +40,8 @@ struct FeeProjectConfig {
 contract REVLoansTests is TestBaseWorkflow, JBTest {
     /// @notice the salts that are used to deploy the contracts.
     bytes32 BASIC_DEPLOYER_SALT = "REVDeployer";
+    bytes32 ERC20_SALT = "REV_TOKEN";
+
     REVDeployer BASIC_DEPLOYER;
     JB721TiersHook EXAMPLE_HOOK;
 
@@ -47,12 +50,120 @@ contract REVLoansTests is TestBaseWorkflow, JBTest {
     IJB721TiersHookStore HOOK_STORE;
     IJBAddressRegistry ADDRESS_REGISTRY;
 
+    IREVLoans LOANS_CONTRACT;
+
     /// @notice Deploys and tracks suckers for revnets.
     IJBSuckerRegistry SUCKER_REGISTRY;
 
     CTPublisher PUBLISHER;
 
     uint256 FEE_PROJECT_ID;
+    uint256 REVLOAN_ID;
+
+    address USER = makeAddr("user");
+
+    function getFeeProjectConfig() internal view returns (FeeProjectConfig memory) {
+        // Define constants
+        string memory name = "Revnet";
+        string memory symbol = "$REV";
+        string memory projectUri = "ipfs://QmNRHT91HcDgMcenebYX7rJigt77cgNcosvuhX21wkF3tx";
+        uint8 decimals = 18;
+        uint256 decimalMultiplier = 10 ** decimals;
+
+        // The tokens that the project accepts and stores.
+        JBAccountingContext[] memory accountingContextsToAccept = new JBAccountingContext[](1);
+
+        // Accept the chain's native currency through the multi terminal.
+        accountingContextsToAccept[0] = JBAccountingContext({
+            token: JBConstants.NATIVE_TOKEN,
+            decimals: 18,
+            currency: uint32(uint160(JBConstants.NATIVE_TOKEN))
+        });
+
+        // The terminals that the project will accept funds through.
+        JBTerminalConfig[] memory terminalConfigurations = new JBTerminalConfig[](1);
+        terminalConfigurations[0] =
+            JBTerminalConfig({terminal: jbMultiTerminal(), accountingContextsToAccept: accountingContextsToAccept});
+
+        // The project's revnet stage configurations.
+        REVStageConfig[] memory stageConfigurations = new REVStageConfig[](3);
+
+        {
+            REVAutoMint[] memory mintConfs = new REVAutoMint[](1);
+            mintConfs[0] = REVAutoMint({
+                chainId: uint32(block.chainid),
+                count: uint104(70_000 * decimalMultiplier),
+                beneficiary: multisig()
+            });
+
+            stageConfigurations[0] = REVStageConfig({
+                startsAtOrAfter: uint40(block.timestamp),
+                autoMints: mintConfs,
+                splitPercent: 2000, // 20%
+                initialIssuance: uint112(1000 * decimalMultiplier),
+                issuanceDecayFrequency: 90 days,
+                issuanceDecayPercent: JBConstants.MAX_DECAY_PERCENT / 2,
+                cashOutTaxRate: 6000, // 0.6
+                extraMetadata: 0
+            });
+        }
+
+        stageConfigurations[1] = REVStageConfig({
+            startsAtOrAfter: uint40(stageConfigurations[0].startsAtOrAfter + 720 days),
+            autoMints: new REVAutoMint[](0),
+            splitPercent: 2000, // 20%
+            initialIssuance: 0, // inherit from previous cycle.
+            issuanceDecayFrequency: 180 days,
+            issuanceDecayPercent: JBConstants.MAX_DECAY_PERCENT / 2,
+            cashOutTaxRate: 6000, // 0.6
+            extraMetadata: 0
+        });
+
+        stageConfigurations[2] = REVStageConfig({
+            startsAtOrAfter: uint40(stageConfigurations[1].startsAtOrAfter + (20 * 365 days)),
+            autoMints: new REVAutoMint[](0),
+            splitPercent: 0,
+            initialIssuance: 1, // this is a special number that is as close to max price as we can get.
+            issuanceDecayFrequency: 0,
+            issuanceDecayPercent: 0,
+            cashOutTaxRate: 6000, // 0.6
+            extraMetadata: 0
+        });
+
+        REVLoanSource[] memory _loanSources = new REVLoanSource[](1);
+        _loanSources[0] = REVLoanSource({token: JBConstants.NATIVE_TOKEN, terminal: jbMultiTerminal()});
+
+        // The project's revnet configuration
+        REVConfig memory revnetConfiguration = REVConfig({
+            description: REVDescription(name, symbol, projectUri, ERC20_SALT),
+            baseCurrency: uint32(uint160(JBConstants.NATIVE_TOKEN)),
+            splitOperator: multisig(),
+            stageConfigurations: stageConfigurations,
+            loanSources: _loanSources,
+            loans: address(LOANS_CONTRACT)
+        });
+
+        // The project's buyback hook configuration.
+        REVBuybackPoolConfig[] memory buybackPoolConfigurations = new REVBuybackPoolConfig[](1);
+        buybackPoolConfigurations[0] = REVBuybackPoolConfig({
+            token: JBConstants.NATIVE_TOKEN,
+            fee: 10_000,
+            twapWindow: 2 days,
+            twapSlippageTolerance: 9000
+        });
+        REVBuybackHookConfig memory buybackHookConfiguration =
+            REVBuybackHookConfig({hook: IJBBuybackHook(address(0)), poolConfigurations: buybackPoolConfigurations});
+
+        return FeeProjectConfig({
+            configuration: revnetConfiguration,
+            terminalConfigurations: terminalConfigurations,
+            buybackHookConfiguration: buybackHookConfiguration,
+            suckerDeploymentConfiguration: REVSuckerDeploymentConfig({
+                deployerConfigurations: new JBSuckerDeployerConfig[](0),
+                salt: keccak256(abi.encodePacked("REV"))
+            })
+        });
+    }
 
     function setUp() public override {
         super.setUp();
@@ -74,10 +185,41 @@ contract REVLoansTests is TestBaseWorkflow, JBTest {
         BASIC_DEPLOYER = new REVDeployer{salt: BASIC_DEPLOYER_SALT}(
             jbController(), SUCKER_REGISTRY, FEE_PROJECT_ID, HOOK_DEPLOYER, PUBLISHER
         );
+
+        LOANS_CONTRACT = new REVLoans(jbProjects(), FEE_PROJECT_ID, permit2());
+
+        // Approve the basic deployer to configure the project.
+        vm.prank(address(multisig()));
+        jbProjects().approve(address(BASIC_DEPLOYER), FEE_PROJECT_ID);
+
+        // Build the config.
+        FeeProjectConfig memory feeProjectConfig = getFeeProjectConfig();
+
+        // Empty hook config.
+        REVDeploy721TiersHookConfig memory tiered721HookConfiguration;
+
+        // Configure the project.
+        REVLOAN_ID = BASIC_DEPLOYER.deployFor({
+            revnetId: FEE_PROJECT_ID,
+            configuration: feeProjectConfig.configuration,
+            terminalConfigurations: feeProjectConfig.terminalConfigurations,
+            buybackHookConfiguration: feeProjectConfig.buybackHookConfiguration,
+            suckerDeploymentConfiguration: feeProjectConfig.suckerDeploymentConfiguration
+        });
+
+        // Give Eth for the user experience
+        vm.deal(USER, 100e18);
     }
 
     function test_is_setup() public {
         assertGt(uint160(address(jbDirectory())), uint160(0));
         assertGt(FEE_PROJECT_ID, 0);
+        assertGt(jbProjects().count(), 0);
+        assertGt(REVLOAN_ID, 0);
+    }
+
+    function test_pay() public {
+        vm.prank(USER);
+        uint256 tokens = jbMultiTerminal().pay{value: 1e18}(REVLOAN_ID, JBConstants.NATIVE_TOKEN, 1e18, USER, 0, "", "");
     }
 }
